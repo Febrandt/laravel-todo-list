12fd3ced4875f189e0a955a5f7ec5503d96a67e9
Febrandt <fernandombrandt@gmail.com>
2025-03-20 17:47:30 -0300

    Setup

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased text-zinc-900">
+    <div class="flex flex-col items-center min-h-screen pt-6 bg-zinc-100 sm:justify-center sm:pt-0 dark:bg-zinc-950">
+
+        <div
+            class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md dark:bg-zinc-800 sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
+</html>
